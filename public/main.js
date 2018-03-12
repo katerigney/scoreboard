6b83b9a993ca687cb17b8e86ee61384b1d88aa13
Rigney <krigney@proforma.com>
2018-03-12 14:45:03 -0400

    content, layout and counters completed.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,29 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+
+let count_team1 = 0;
+
+const addScore1 = () => {
+  count_team1 = count_team1 + 1
+  const teamScore1add = document.querySelector(".team1-score");
+  teamScore1add.textContent = count_team1;
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const subScore1 = () => {
+  count_team1 = count_team1 - 1;
+  const teamScore1sub = document.querySelector(".team1-score");
+  teamScore1sub.textContent = count_team1;
+}
+
+
+let count_team2 = 0;
+
+const addScore2 = () => {
+  count_team2 = count_team2 + 1
+  const teamScore2add = document.querySelector(".team2-score");
+  teamScore2add.textContent = count_team2;
+}
+
+const subScore2 = () => {
+  count_team2 = count_team2 - 1;
+  const teamScore2sub = document.querySelector(".team2-score");
+  teamScore2sub.textContent = count_team2;
+}
